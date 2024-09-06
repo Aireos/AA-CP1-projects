05a12d1344ce3d1de36f212e15a35dfd1bdab432
Alex Anderson <alex.anderson@ucas-edu.net>
2024-09-06 10:51:43 -0600

    fixed if name==

--- Personal Work/main.py
+++ Personal Work/main.py
@@ -1,4 +1,2 @@
 name=input("Hello, what is your name? (this can be changed later): ")
-if(name)== "alex":
-    print("that name is already taken")
-    #restart
+if name==("alex"): print("that name is already taken") 
