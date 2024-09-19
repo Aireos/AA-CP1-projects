1955d32f453fd3404ba42c59134a8487ae71002b
Alex <alex.anderson@ucas-edu.net>
2024-09-18 22:49:38 -0600

    making personal

--- Personal Work/main.py
+++ Personal Work/main.py
@@ -1,2 +1,13 @@
+import random
+chosenames = ""
+beggining_class = random
 name=input("Hello, what is your name? (this can be changed later): ")
-if name==("alex"): print("that name is already taken") 
+if name == chosenames:
+    print("This name is already taken")
+makingsure = input("Just making sure, Your name is""" )
+if input == "yes":
+    print("Welcome to the world of floried")
+    print(beggining_class)
+if input == "no":
+    print("Please re-enter name")
+else: print("please enter yes or no")
