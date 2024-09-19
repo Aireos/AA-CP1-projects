5d5ac9753e79a22e8931c4193f7069bac87d751a
Alex <alex.anderson@ucas-edu.net>
2024-09-18 22:20:26 -0600

    helo

--- First_Project/main.py
+++ First_Project/main.py
@@ -1,4 +1,8 @@
 import random
 
-num = random.randint(1,20)
-print("Hello world", num) 
+num = random.randint(1,999999999)
+print("Hello Player", "#",num) 
+gamertag = input("What is your gamertag: ")
+print("hello",gamertag, ", welcome to aix")
+mainweaponarea = input("would you like to be ranged, magic, melee or joat(jack of all trades): ")
+if mainweaponarea : "ranged" : op2 = input("are you sure you want to be ranged?: ")
