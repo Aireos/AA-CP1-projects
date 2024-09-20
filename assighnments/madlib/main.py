6641bd6f3fa5fca1c6ed89813051fa1146a1f7b7
Alex Anderson <alex.anderson@ucas-edu.net>
2024-09-20 10:35:08 -0600

    finished madlib

--- assighnments/madlib/main.py
+++ assighnments/madlib/main.py
@@ -1,6 +1,12 @@
-number = input("Type a number 1-10: ")
-hair_coler = input("Type a hair coler: ")
-animal = input("Type a animal: ")
-place = input("Type a place: ")
-name = input("Type a name: ")
-print("Hi, i'm", name, "and I have", hair_coler, "hair. I like", animal + "s that have", hair_coler, "hair!")
+
+number = input("Type a number:")
+hair_coler = input("Type a hair coler:")
+animal = input("Type a animal:")
+place = input("Type a place:")
+name = input("Type a name:")
+print("Hi, i'm", name, "and I have", hair_coler, "hair. I like", animal + "s that have", hair_coler, "hair too! I live in", place, "and I am", number, "years old. One of my favorite things to do is walk a", animal, "around the block! Thanks for listining in and please ask", number, "peaple to listen too!")
+
+
+
+
+
