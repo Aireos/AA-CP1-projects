a8cf9768357e6293a4289273a109c537d3c15b4f
Alex Anderson <alex.anderson@ucas-edu.net>
2024-09-23 10:23:55 -0600

    made personal converter

---
+++ assighnments/personal converter/main.py
@@ -0,0 +1,24 @@
+
+
+before_name = input("What is your name?: ")
+
+before_age = input("What is your age?: ")
+
+before_height = input("What is your height in meters?: ")
+
+before_favorite = input("What is your favorite number?: ")
+
+after_age = int(before_age)
+
+after_favorite = int(before_favorite)
+
+after_height = float(before_height)
+
+after_name = before_name
+
+print(" ")
+print("Before:", before_name, before_age, before_height, before_favorite)
+print(" ")
+print("After:", after_name, after_age, after_height, after_favorite)
+print(" ")
+
