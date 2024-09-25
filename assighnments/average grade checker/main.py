895a810f0e1febf92521435c34f36c780e4e6257
Alex <alex.anderson@ucas-edu.net>
2024-09-25 16:24:09 -0600

    so close to being finished algorithem

--- assighnments/average grade checker/main.py
+++ assighnments/average grade checker/main.py
@@ -1,10 +1,23 @@
 
+numberofclasses = ''
+grade = ''
+average_grade = ''
 
-def average_alg(numberofclasses, grades):
-    average_grade = ''
+def algorithim(numberofclasses):
+    totalgrade = 0
+    for amount in numberofclasses:
+        totalgrade += (int(input('What is the grade for this class?: ')))      
+    return (totalgrade)
 
 
+numberofclasses = (input('What is the number of classes?: '))
 
-numberofclasses = int(input('What is the amount of classes you want to check?: '))
-for number in numberofclasses:
-    
+totalgrade = algorithim(numberofclasses)
+
+numberofclasses = int(numberofclasses)
+
+average_grade = totalgrade / numberofclasses
+
+print("The average grade is:", average_grade)
+
+
