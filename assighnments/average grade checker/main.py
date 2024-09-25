f5c02ef8af6027ce7cd2fb36b5903c0b605f9cbe
Alex Anderson <alex.anderson@ucas-edu.net>
2024-09-25 10:50:46 -0600

    starting making grade checker

---
+++ assighnments/average grade checker/main.py
@@ -0,0 +1,10 @@
+
+
+def average_alg(numberofclasses, grades):
+    average_grade = ''
+
+
+
+numberofclasses = int(input('What is the amount of classes you want to check?: '))
+for number in numberofclasses:
+    
