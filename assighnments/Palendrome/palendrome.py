d8fa0e051eb5d62d0e616f1a24d7b5b15021bcee
Alex Anderson <alex.anderson@ucas-edu.net>
2024-09-27 10:07:30 -0600

    finised palendrome

--- assighnments/Palendrome/palendrome.py
+++ assighnments/Palendrome/palendrome.py
@@ -1,2 +1,8 @@
 text = input('What do you want to check to see if it is a palendrome?: ')
-    
+reversedtext = text[::-1]
+if text == reversedtext:
+    print()
+    print(text, "is a palendrome")
+else:
+    print()
+    print(text, "is not a palendrome")
