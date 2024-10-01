3ed18fb739d9f420c60132738e2b4f4612e03837
Alex <alex.anderson@ucas-edu.net>
2024-09-30 19:06:21 -0600

    finished example

--- Personal Work/side.py
+++ Personal Work/side.py
@@ -5,6 +5,13 @@ def function_one(prt_one, prt_two):
     prt_three = prt_one * prt_two
     return prt_three
 
-prt_three = function_one(2, 2)
+
+
+
+prt_one = 2
+prt_two = 2
+
+
+prt_three = function_one(prt_one, prt_two)
 
 print(prt_three)
