197d71b7f9df0b001acd4b96bf8b7130435f44b5
Miner1324 <alex.anderson@ucas-edu.net>
2024-10-04 11:20:01 -0600

    Update main.py

--- assighnments/pig latin/main.py
+++ assighnments/pig latin/main.py
@@ -2,8 +2,36 @@
 
 vowel = ['a', 'e', 'i', 'o', 'u']
 
-before = str(input('What word to you want to make into pig latin?: '))
+string = str(input('What word to you want to make into pig latin?: '))
 
-splited = before.split(vowel)
+position_a = (string.find(a)) + 1
+position_e = (string.find(e)) + 1
+position_i = (string.find(i)) + 1
+position_o = (string.find(o)) + 1
+position_u = (string.find(u)) + 1
 
-print(splited)
+if position_a == 0:
+  position_a = 100000000
+if position_e == 0:
+  position_e = 100000000
+if position_i == 0:
+  position_i = 100000000
+if position_o == 0:
+  position_o = 100000000
+if position_u == 0:
+  position_u = 100000000
+
+if position_a < position_e, position_i, position_o or position_u:
+  firstvowel = position_a
+if position_e < position_a, position_i, position_o or position_u:
+  firstvowel = position_e
+if position_i < position_e, position_a, position_o or position_u:
+  firstvowel = position_i
+if position_o < position_e, position_i, position_a or position_u:
+  firstvowel = position_o
+if position_u < position_e, position_i, position_o or position_a:
+  firstvowel = position_u
+
+split = string.split(firstvowel)
+print(split)
+
