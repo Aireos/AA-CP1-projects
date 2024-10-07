89f00ad9583ae344512e4b1deb1891ef4a1f4e75
Alex Anderson <alex.anderson@ucas-edu.net>
2024-10-07 10:52:06 -0600

    number gusser in progress

---
+++ assighnments/number gusser/main.py
@@ -0,0 +1,17 @@
+#Make code where program generates a number between 1 and 10 and tell the user if they got it right.
+
+#it should first tell them what it is, then have programm randomly generate number, then it will tell them to guess a number one through ten and tell them if it is correct.
+
+#need to import random, make random number generater and get eh number in int form of what they gussed
+
+#Import random (for generater)
+#print "number gusser game" at the top right as code starts
+#Ask for their guess (set to guess varaible)
+#using random, generate number 1-10 (set to number varaible)
+#set that if guess == number: tell them that they were correct
+#set else: tell them that they were incorrect
+
+import random
+print("Welcome to the number guessing game!")
+
+
