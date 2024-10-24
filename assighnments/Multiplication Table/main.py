1c912907f2246f02a490ad0053c3542940bf2cd5
Alex Anderson <alex.anderson@ucas-edu.net>
2024-10-24 10:52:30 -0600

    maker sure to add original number

---
+++ assighnments/Multiplication Table/main.py
@@ -0,0 +1,127 @@
+
+
+import sys
+
+import time
+
+def len_dash(number):
+    for x in number:
+        print("-",end="",flush=False)
+    print("",flush=False)
+
+
+number = int(input("What would you like multiples of?: "))
+
+print("These are the multiples of", number,'(1-12)')
+
+(time.sleep(0.5))
+
+number = number*1
+len_dash(str(number)+"1234")
+
+(time.sleep(0.5))
+
+print("|",(number),"|")
+
+(time.sleep(0.5))
+
+number = number*2
+len_dash(str(number)+"1234")
+
+(time.sleep(0.5))
+
+print("|",(number),"|")
+
+(time.sleep(0.5))
+
+number = number*3
+len_dash(str(number)+"1234")
+
+(time.sleep(0.5))
+
+print("|",(number),"|")
+
+(time.sleep(0.5))
+
+number = number*4
+len_dash(str(number)+"1234")
+
+(time.sleep(0.5))
+
+print("|",(number),"|")
+
+(time.sleep(0.5))
+
+number = number*5
+len_dash(str(number)+"1234")
+
+(time.sleep(0.5))
+
+print("|",(number),"|")
+
+(time.sleep(0.5))
+
+number = number*5
+len_dash(str(number)+"1234")
+
+(time.sleep(0.5))
+
+print("|",(number),"|")
+
+(time.sleep(0.5))
+
+number = number*1
+len_dash(str(number)+"1234")
+
+(time.sleep(0.5))
+
+print("|",(number),"|")
+
+(time.sleep(0.5))
+
+number = number*1
+len_dash(str(number)+"1234")
+
+(time.sleep(0.5))
+
+print("|",(number),"|")
+
+(time.sleep(0.5))
+
+number = number*1
+len_dash(str(number)+"1234")
+
+(time.sleep(0.5))
+
+print("|",(number),"|")
+
+(time.sleep(0.5))
+
+number = number*1
+len_dash(str(number)+"1234")
+
+(time.sleep(0.5))
+
+print("|",(number),"|")
+
+(time.sleep(0.5))
+
+number = number*1
+len_dash(str(number)+"1234")
+
+(time.sleep(0.5))
+
+print("|",(number),"|")
+
+(time.sleep(0.5))
+
+number = number*1
+len_dash(str(number)+"1234")
+
+(time.sleep(0.5))
+
+print("|",(number),"|")
+
+
+
+
