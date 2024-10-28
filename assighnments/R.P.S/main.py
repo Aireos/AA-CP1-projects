82da97a6d854d050f7d70885c2a7c82525ddf391
Alex Anderson <alex.anderson@ucas-edu.net>
2024-10-28 10:48:47 -0600

    finished paper

--- assighnments/R.P.S/main.py
+++ assighnments/R.P.S/main.py
@@ -2,12 +2,147 @@
 
 import time
 import random
-placements = []
-print("Welcome to tick tac toe!")
+gamedone = False
+score = 0
+(time.sleep(0.25))
+
+print("Welcome to Rock, Paper, Scissors!")
 print()
-rounds = int(input("How many rounds would you like to play?: "))
+while True:
+
+    computer_input = random.randint(1,3)
+
+    if computer_input == 1:
+        computer_input = "rock"
+
+    if computer_input == 2:
+        computer_input = "paper"
+
+    if computer_input == 3:
+        computer_input = "scissor"
+    
+
+    user_input = input("Would you like to do rock, paper, or scissor?: ")
+    user_input = user_input.lower()
+
+
+    if computer_input == "rock" and user_input == "rock":
+        print()
+        print("The computer chose rock")
+        print()
+        print("You both chose rock.")
+        print()
+        continue
+
+    if computer_input == "rock" and user_input == "paper":
+        print()
+        print("The computer chose rock")
+        print()
+        print("You won!")
+        score += 1
+        print()
+        choice = input("Would you like to play again? (yes or no): ")
+        print()
+        if choice == "yes":
+            continue
+        if choice == "no":
+            break
+
+    if computer_input == "rock" and user_input == "scissor":
+        print()
+        print("The computer chose rock")
+        print()
+        print("You lost")
+        score += -1
+        print()
+        choice = input("Would you like to play again? (yes or no): ")
+        print()
+        if choice == "yes":
+            continue
+        if choice == "no":
+            break 
+
+
+    if computer_input == "paper" and user_input == "rock":
+        print()
+        print("The computer chose paper")
+        print()
+        print("You lost")
+        score += -1
+        print()    
+        choice = input("Would you like to play again? (yes or no): ")
+        print()
+        if choice == "yes":
+            continue
+        if choice == "no":
+            break
+
+    if computer_input == "paper" and user_input == "paper":
+        print()
+        print("The computer chose paper")
+        print()
+        print("You both chose paper.")
+        print()
+        continue
+
+    if computer_input == "paper" and user_input == "scissor":
+        print()
+        print("The computer chose paper")
+        print()
+        print("You won!")
+        score += 1
+        print()
+        choice = input("Would you like to play again? (yes or no): ")
+        print()
+        if choice == "yes":
+            continue
+        if choice == "no":
+            break
+
+
+    if computer_input == "scissor" and user_input == "rock":
+        print()
+        print("The computer chose scissor")
+        print()
+        print("You won!")
+        score += 1
+        print()
+        choice = input("Would you like to play again? (yes or no): ")
+        print()
+        if choice == "yes":
+            continue
+        if choice == "no":
+            break
+
+    if computer_input == "scissor" and user_input == "paper":
+        print()
+        print("The computer chose scissor")
+        print()
+        print("You lost")
+        score += -1
+        print()
+        choice = input("Would you like to play again? (yes or no): ")
+        print()
+        if choice == "yes":
+            continue
+        if choice == "no":
+            break
+
+    if computer_input == "scissor" and user_input == "scissor":
+        print()
+        print("The computer chose scissor")
+        print()
+        print("You both chose scissor.")
+        print()
+        continue
+
+
+    else: 
+        print()
+        print("Invalid entry, try again")
+        print()
+        continue
+print("you score was:",score)
 print()
-while rounds >= 1:
-    placement = int(input("What place on the bourd do you want to place a X at?: "))
-    if placement in placements:
-        print("this ")
+print("Bye!")
+print()
