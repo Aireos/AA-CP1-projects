c82b86d6e9f4c0040924a279ae82257c1affcfe4
Miner1324 <alex.anderson@ucas-edu.net>
2024-10-29 10:48:29 -0600

    loop made.........................................

---
+++ assighnments/counting up and down/main.py
@@ -0,0 +1,117 @@
+import time
+while True:
+    number = 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number + 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+    number = number - 1
+    print(number)
+    (time.sleep(0.5))
+
