c20117b8eda7d6a78ca3b20fc9de9377bee14620
Miner1324 <alex.anderson@ucas-edu.net>
2024-11-05 10:37:29 -0700

    simple quiz game

--- assighnments/Simple quiz game/main.py
+++ assighnments/Simple quiz game/main.py
@@ -1,6 +1,8 @@
 
 correct = False
 
+N_O_C = 0
+
 def C_O_I(user_answer, answer):
 
     correct = False
@@ -16,10 +18,13 @@ def C_O_I(user_answer, answer):
         print()
         print("You got it wrong.")
         print()
+        correct = False
         return correct
+    
 print()
 print("math quiz time!")
 print()
+
 
 #first question
 user_answer = int(input("What is 2+1 (1, 2, 3 or 4): "))
@@ -29,6 +34,7 @@ correct = C_O_I(user_answer, answer)
 while True:
     #second question hard
     if correct == True:
+        N_O_C += 1
         user_answer = int(input("What is 2-1 (1, 2, 3 or 4): "))
         answer = 1
         correct = C_O_I(user_answer, answer)
@@ -44,8 +50,9 @@ while True:
 while True:
     #third question hard
     if correct == True:
+        N_O_C += 1
         user_answer = int(input("What is 4/2 (1, 2, 3 or 4): "))
-        answer = 1
+        answer = 2
         correct = C_O_I(user_answer, answer)
         break
 
@@ -59,29 +66,37 @@ while True:
 while True:
     #fourth question hard
     if correct == True:
+        N_O_C += 1
         user_answer = int(input("What is 2*2 (1, 2, 3 or 4): "))
-        answer = 1
+        answer = 4
         correct = C_O_I(user_answer, answer)
         break
 
     #fourth question easy
     elif correct == False:
         user_answer = int(input("What is 2/2 (1, 2, 3 or 4): "))
-        answer = 2
+        answer = 1
         correct = C_O_I(user_answer, answer)
         break
 
 while True:
     #fith question hard
     if correct == True:
-        user_answer = int(input("What is 2-1 (1, 2, 3 or 4): "))
-        answer = 1
+        N_O_C += 1
+        user_answer = int(input("What is (1.50000015*3.333333)-1 (1, 2, 3 or 4): "))
+        answer = 4
         correct = C_O_I(user_answer, answer)
+        if correct == True:
+            N_O_C += 1
         break
 
     #fith question easy
     elif correct == False:
-        user_answer = int(input("What is 1+1 (1, 2, 3 or 4): "))
-        answer = 2
+        user_answer = int(input("What is 5/5 (1, 2, 3 or 4): "))
+        answer = 1
         correct = C_O_I(user_answer, answer)
+        if correct == True:
+            N_O_C += 1
         break
+
+print("You got:", N_O_C, "/ 5 correct.")
