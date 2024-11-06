939221867b5e6d31073ec6d614d50be352192568
Miner1324 <alex.anderson@ucas-edu.net>
2024-11-06 10:52:15 -0700

    started tic

---
+++ assighnments/tic tac toe/main.py
@@ -0,0 +1,72 @@
+import random
+spacer = "~~~~~~~~~~~~~"
+row1 = ["|"," ","1"," ","|"," ","2"," ","|"," ","3"," ","|"]
+row2 = ["|"," ","4"," ","|"," ","5"," ","|"," ","6"," ","|"]
+row3 = ["|"," ","7"," ","|"," ","8"," ","|"," ","9"," ","|"]
+
+def gameboard():
+    print()
+    print()
+    print()
+    print()
+    print()
+    print(spacer)
+    print(''.join(row1))
+    print(spacer)
+    print(''.join(row2))
+    print(spacer)
+    print(''.join(row3))
+    print(spacer)
+
+while True:
+    gameboard()
+    placment = int(input("What place on the bourd do you want to place a X on? (1-9): "))
+    if placment in row1 or row2 or row3:
+        if placment == 1:
+            row1[2] = "X"
+        if placment == 2:
+            row1[6] = "X"
+        if placment == 3:
+            row1[10] = "X"
+        if placment == 4:
+            row2[2] = "X"
+        if placment == 5:
+            row2[6] = "X"
+        if placment == 6:
+            row2[10] = "X"
+        if placment == 7:
+            row3[2] = "X"
+        if placment == 8:
+            row3[6] = "X"
+        if placment == 9:
+            row3[10] = "X"        
+        gameboard()
+        break
+    else:
+        print("That placement is already taken, try again.")
+
+while True:
+    placment = random(1,9)
+    if placment in row1 or row2 or row3:
+        if placment == 1:
+            row1[2] = "X"
+        if placment == 2:
+            row1[6] = "X"
+        if placment == 3:
+            row1[10] = "X"
+        if placment == 4:
+            row2[2] = "X"
+        if placment == 5:
+            row2[6] = "X"
+        if placment == 6:
+            row2[10] = "X"
+        if placment == 7:
+            row3[2] = "X"
+        if placment == 8:
+            row3[6] = "X"
+        if placment == 9:
+            row3[10] = "X"        
+        gameboard()
+        break
+    else:
+        print("That placement is already taken, try again.")
