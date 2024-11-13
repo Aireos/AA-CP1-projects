b02077a947ab77003cf069b1ada4c361c077c022
Miner1324 <alex.anderson@ucas-edu.net>
2024-11-13 10:51:37 -0700

    working on bot

--- assighnments/tic tac toe/main.py
+++ assighnments/tic tac toe/main.py
@@ -19,54 +19,55 @@ def gameboard():
     print(spacer)
 
 while True:
-    gameboard()
-    placment = int(input("What place on the bourd do you want to place a X on? (1-9): "))
-    if placment in row1 or row2 or row3:
-        if placment == 1:
-            row1[2] = "X"
-        if placment == 2:
-            row1[6] = "X"
-        if placment == 3:
-            row1[10] = "X"
-        if placment == 4:
-            row2[2] = "X"
-        if placment == 5:
-            row2[6] = "X"
-        if placment == 6:
-            row2[10] = "X"
-        if placment == 7:
-            row3[2] = "X"
-        if placment == 8:
-            row3[6] = "X"
-        if placment == 9:
-            row3[10] = "X"        
+    while True:
         gameboard()
-        break
-    else:
-        print("That placement is already taken, try again.")
+        placment = int(input("What place on the bourd do you want to place a X on? (1-9): "))
+        if placment in row1 or row2 or row3:
+            if placment == 1:
+                row1[2] = "X"
+            if placment == 2:
+                row1[6] = "X"
+            if placment == 3:
+                row1[10] = "X"
+            if placment == 4:
+                row2[2] = "X"
+            if placment == 5:
+                row2[6] = "X"
+            if placment == 6:
+                row2[10] = "X"
+            if placment == 7:
+                row3[2] = "X"
+            if placment == 8:
+                row3[6] = "X"
+            if placment == 9:
+                row3[10] = "X"        
+            gameboard()
+            break
+        else:
+            print("That placement is already taken, try again.")
 
-while True:
-    placment = random(1,9)
-    if placment in row1 or row2 or row3:
-        if placment == 1:
-            row1[2] = "X"
-        if placment == 2:
-            row1[6] = "X"
-        if placment == 3:
-            row1[10] = "X"
-        if placment == 4:
-            row2[2] = "X"
-        if placment == 5:
-            row2[6] = "X"
-        if placment == 6:
-            row2[10] = "X"
-        if placment == 7:
-            row3[2] = "X"
-        if placment == 8:
-            row3[6] = "X"
-        if placment == 9:
-            row3[10] = "X"        
-        gameboard()
-        break
-    else:
-        print("That placement is already taken, try again.")
+    while True:
+        number = random.randint(1,9)
+        if number in row1 or row2 or row3:
+            if number == 1:
+                row1[2] = "O"
+            if number == 2:
+                row1[6] = "O"
+            if number == 3:
+                row1[10] = "O"
+            if number == 4:
+                row2[2] = "O"
+            if number == 5:
+                row2[6] = "O"
+            if number == 6:
+                row2[10] = "O"
+            if number == 7:
+                row3[2] = "O"
+            if number == 8:
+                row3[6] = "O"
+            if number == 9:
+                row3[10] = "O"        
+            gameboard()
+            break
+    
+    if row
