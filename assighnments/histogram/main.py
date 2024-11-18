7a364f9fc8d3aa4c25fe0ef08ad71ee91c5fe6ca
Miner1324 <alex.anderson@ucas-edu.net>
2024-11-18 10:51:01 -0700

    started histogram

---
+++ assighnments/histogram/main.py
@@ -0,0 +1,15 @@
+input1 = int(input("What is your first input?: "))
+input2 = int(input("What is your second input?: "))
+input3 = int(input("What is your third input?: "))
+input4 = int(input("What is your fourth input?: "))
+input5 = int(input("What is your fifth input?: "))
+input6 = int(input("What is your sixth input?: "))
+amount1 = 0
+amount1 = 0
+amount1 = 0
+amount1 = 0
+amount1 = 0
+amount1 = 0
+def changer(input, amount):
+    for x in input:
+        amount =+ 1
