98449823f83434101e691a1940c19bf4c6a447b6
Miner1324 <alex.anderson@ucas-edu.net>
2024-11-20 10:26:33 -0700

    fixed histogram

--- assighnments/histogram/main.py
+++ assighnments/histogram/main.py
@@ -1,63 +1,38 @@
 nospace = ""
 
-input1 = (input("What is your first input?: "))
-input2 = (input("What is your second input?: "))
-input3 = (input("What is your third input?: "))
-input4 = (input("What is your fourth input?: "))
-input5 = (input("What is your fifth input?: "))
-input6 = (input("What is your sixth input?: "))
+input1 = input("What is your first input?: ")
+input2 = input("What is your second input?: ")
+input3 = input("What is your third input?: ")
+input4 = input("What is your fourth input?: ")
+input5 = input("What is your fifth input?: ")
+input6 = input("What is your sixth input?: ")
 
-amount1 = []
-amount2 = []
-amount3 = []
-amount4 = []
-amount5 = []
-amount6 = []
+amounts = [input1, input2, input3, input4, input5, input6]
+outputs = ['','','','','','']
+placment = -1
 
-for i in input1:
+for i in amounts:
+
+    placment += 1
+    amount = []
     i = int(i)
+
     while i >= 1:
         i -= 1
-        amount1 += "*"
-amount1 = nospace.join(amount1)
-print("1:",amount1)
+        amount += '*'
+    number = placment + 1
+    number = str(number)
+    print(number + ":", nospace.join(amount))
 
-for i in input2:
-    i = int(i)
-    while i >= 1:
-        i -= 1
-        amount2 += "*"
-amount2 = nospace.join(amount2)
-print("2:",amount2)
 
-for i in input3:
-    i = int(i)
-    while i >= 1:
-        i -= 1
-        amount3 += "*"
-amount3 = nospace.join(amount3)
-print("3:",amount3)
+    
+    
 
-for i in input4:
-    i = int(i)
-    while i >= 1:
-        i -= 1
-        amount4 += "*"
-amount4 = nospace.join(amount4)
-print("4:",amount4)
 
-for i in input5:
-    i = int(i)
-    while i >= 1:
-        i -= 1
-        amount5 += "*"
-amount5 = nospace.join(amount5)
-print("5:",amount5)
-
-for i in input6:
-    i = int(i)
-    while i >= 1:
-        i -= 1
-        amount6 += "*"
-amount6 = nospace.join(amount6)
-print("6:",amount6)
+#for i in amounts:
+#   i = int(i)
+#   while i >= 1:
+#       i -= 1
+#       amount1 += "*"
+#amount1 = nospace.join(amount1)
+#print("1:",amount1)
