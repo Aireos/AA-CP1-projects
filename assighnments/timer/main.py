9cae83a340aabba3b0dca41ffeccd9ee030dec9d
Aireos <alex.anderson@ucas-edu.net>
2025-02-05 09:59:53 -0700

    did thing

--- assighnments/timer/main.py
+++ assighnments/timer/main.py
@@ -3,6 +3,6 @@ import time
 time_left = int(input("How long (in seconds) do you want you timer to be?: "))
 print(time_left)
 while time_left > 0:
-    (time.sleep(1))
+    (time.sleep(0.00000000000000001))
     time_left = time_left - 1
     print(time_left)
